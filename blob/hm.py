131d35f59395ddc258eb2360f66ba12aafde6397
sam00002135 <baih7069@gmail.com>
2023-09-23 10:05:14 +0800

    hm.py

--- blob/hm.py
+++ blob/hm.py
@@ -1,21 +1,20 @@
 from datetime import datetime
 
-def fibonacci(n):
-    if n < 0:
-        raise ValueError("輸入不是正整數")
-    if not fib[n] is None:
-        return fib[n]
-    fib[n] = fibonacci(n - 1) + fibonacci(n - 2)
-    return fib[n]
-
 num = int(input('請輸入求第幾項費氏數列? '))
 
-fib = [None] * (num + 1)
-fib[0] = 0
-fib[1] = 1
+# 初始的前兩個費波那契數列值
+a, b = 0, 1
+
+if num == 0:
+    result = 0
+elif num == 1:
+    result = 1
+else:
+    for i in range(2, num + 1):
+        result = a + b
+        a, b = b, result
 
 startTime = datetime.now()
-result = fibonacci(num)
 endTime = datetime.now()
 seconds = endTime - startTime
 
