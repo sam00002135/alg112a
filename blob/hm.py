4a7284c98319b49aa46767d766fa9e20cda3a9dd
sam00002135 <baih7069@gmail.com>
2023-09-23 12:46:23 +0800

    hm.py

--- blob/hm.py
+++ blob/hm.py
@@ -2,8 +2,7 @@ from datetime import datetime
 
 num = int(input('請輸入求第幾項費氏數列? '))
 
-a = 0
-b = 1
+a, b = 0, 1
 
 if num == 0:
     result = 0
@@ -21,11 +20,10 @@ seconds = endTime - startTime
 print(f'fibonacci({num})={result}')
 print(f'time:{seconds}')
 
-
 # num = int(input('請輸入求第幾項費氏數列? '))
 # a = 1
 # b = 1
 # print(1,a)
 # for i in range(2,num+1):
 #     a,b = b,a+b
-#    print(i,a)
+#     print(i,a)
